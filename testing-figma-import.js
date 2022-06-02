478bebba6b63042affb08353a917ef2a1c8f0632
afweb <af_web@inbox.ru>
2022-06-02 10:53:07 +0300

    NEW: first commit

---
+++ testing-figma-import.js
@@ -0,0 +1,21 @@
+const PORT = 7000;
+
+var express = require("express");
+var app = express();
+var axios = require("axios");
+
+app.get("/sitecontent", function (req, res) {
+
+  // получение sitecontent 
+  axios.get('http://localhost:8000/vfcLzhPe3Aowdak3AZPXK8/636:3/sitecontent', {}).then(function (response) {
+    console.log(response);
+    res.send(response.data);
+  }).catch(function (error) {
+    console.error(error);
+  });
+
+});
+
+app.listen(PORT, function () {
+  console.log("Express is listening at port: " + PORT);
+});
