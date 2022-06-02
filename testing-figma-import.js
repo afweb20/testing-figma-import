56f362393e46cfc25abfdbe89c1fb80869cc0105
afweb <af_web@inbox.ru>
2022-06-02 12:02:03 +0300

    bootstrap added

--- testing-figma-import.js
+++ testing-figma-import.js
@@ -4,16 +4,22 @@ var express = require("express");
 var app = express();
 var axios = require("axios");
 
-app.get("/sitecontent", function (req, res) {
+// app.get("/sitecontent", function (req, res) {
 
-  // получение sitecontent 
-  axios.get('http://localhost:8000/vfcLzhPe3Aowdak3AZPXK8/636:3/sitecontent', {}).then(function (response) {
-    console.log(response);
-    res.send(response.data);
-  }).catch(function (error) {
-    console.error(error);
-  });
+//   // получение sitecontent 
+//   axios.get('http://localhost:8000/vfcLzhPe3Aowdak3AZPXK8/636:3/sitecontent', {}).then(function (response) {
+//     console.log(response);
+//     res.send(response.data);
+//   }).catch(function (error) {
+//     console.error(error);
+//   });
 
+// });
+
+app.set('view engine', 'pug');
+
+app.get("/", function (req, res) {
+  res.render("index", {title: "Hey!", message: "hello"});
 });
 
 app.listen(PORT, function () {
